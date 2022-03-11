6d315e22022813a7b2cf07977f097ee55f612007
willyheyeeee <100400794+willyheyeeee@users.noreply.github.com>
2022-03-11 14:40:17 +0800

    Add files via upload

--- r.py
+++ r.py
@@ -1,27 +1,60 @@
-from PIL import Image
-import os
 import requests as rqs
 from bs4 import BeautifulSoup as bsp
-def get_input():
-    ipt = input('Please input the keyward: ')
-    ret = ''
-    for i in ipt.strip().split():
-        ret += i+'+'
-        return ret[:-1]
+blocks = {  # texts // str
+    'title': '',
+    'content': '', #0
+    'input': '', #1
+    'output': '', #2
+    'sample_input': '', #3
+    'sample_output': '', #4
+    'hint': '', #5
+}
+def process_textp(content, pkeys: list, name: str): # 題目敘述/輸入說明/說出說明/提示
+    pkeys_id = 0
+    for id in [0, 1, 2, 5]:
+        i = content[id]
+        ok = False
+        data = i.find_all(name=name)
+        if len(data) != 0:
+            for j in data:
+                img = j.find(name='img')
+                if not (img is None):
+                    blocks[pkeys[pkeys_id]] +=(({img.get('src')})+'\n\n')
+                else :
+                    blocks[pkeys[pkeys_id]] += (j.text.strip('#')+'\n\n')
+                ok = True
+        else:
+            blocks[pkeys[pkeys_id]] += i.text.strip('#') + '\n\n'
+            ok=True
+        if ok:
+            pkeys_id += 1
+def process_textpre(content, pkeys: list, name: str):
+    pkeys_id = 0
+    for i in content:
+        ok = False
+        text = '```\n'
+        for j in i.find_all(name=name):
+            text_list = j.text.strip().split('\n')
+            for t in text_list:
+                text += t.strip() + '\n'
+            ok = True
+
+        if ok:
+            blocks[pkeys[pkeys_id]] += text + '```\n'
+            pkeys_id += 1
+def process_title(content): #題目名稱
+    material = content.find_all(name='div', attrs={'class', 'h1'})
+    blocks['title'] = material[0].text[0:5] + ' ' + material[0].find(name='span').text
 def main():
-    keyword = get_input()
-    response = rqs.get('https://imgur.com/search?q={}'.format(keyword))
+    pid = 'a520'
+    url = 'http://203.64.191.163/ShowProblem?problemid={}'
+    response = rqs.get(url.format(pid))
     html = bsp(response.text, 'html.parser')
-    element = html.find_all(name='div', attrs={'class', 'cards'})[0]
-    pictures = element.find_all(name='img')
-    picture_urls=[]
-    for i in pictures:
-        picture_urls.append(i.get('src')[14:])
-    for i in picture_urls:
-        res_pic = rqs.get(f'https://i.imgur.com/{i}', stream=True, timeout=5)
-        image = Image.open(res_pic.raw)
-        image.show()
-        os.system('pause')
-    
+    content = html.find_all(name='div', attrs={'class', 'problembox'})
+    process_textp(content, ['content', 'input', 'output', 'hint'], 'p')
+    process_textpre(content, ['sample_input', 'sample_output'], 'pre')
+    process_title(html)
+    print(blocks)
+   
 if __name__ == '__main__':
     main()
